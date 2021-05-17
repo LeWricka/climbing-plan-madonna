f81206333fa91f5c9519671f011dbbed2b2db77a
Ekaitz Torregrosa <ekaitz.torregrosa@540deg.com>
2021-05-17 16:05:23 +0200

    [16] - Cambiar de modelo Event a Routine

--- src/app/Http/Controllers/CalendarController.php
+++ src/app/Http/Controllers/CalendarController.php
@@ -2,20 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Event;
+use App\Models\Routine;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Google\Type\Date;
 
 class CalendarController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $event1 = new Event(["title" => "First Rutine", "start" => "2021-05-14", "end" => "2021-05-16", "color" => "#e04f1a"]);
-        $event2 = new Event(["title" => "Second Rutine", "start" => "2021-05-17", "end" => "2021-05-19", "color" => "#82b54b"]);
-        $event3 = new Event(["title" => "Third Rutine", "start" => "2021-05-20", "color" => "#ffb119"]);
 
-        $events = [];
-        array_push($events, $event1, $event2, $event3);
-        return view('pages/mainCalendar', ['events' => $events]);
+        $routine1 = new Routine("First Routine","2021-05-14", "2021-05-16", "#e04f1a");
+        $routine2 = new Routine("Second Routine", "2021-05-17", "2021-05-19", "#82b54b");
+        $routine3 = new Routine("Third Routine","2021-05-20", "2021-05-20", "#ffb119");
+        $routine4 = new Routine("Final Routine","2021-05-22", "2021-05-24", "#e04f1a");
+
+
+        $routines = [];
+        array_push($routines,$routine1->routineToJson(), $routine2->routineToJson(), $routine3->routineToJson(), $routine4->routineToJson());
+
+        return view('pages/mainCalendar', ['routines' => $routines]);
     }
 }
