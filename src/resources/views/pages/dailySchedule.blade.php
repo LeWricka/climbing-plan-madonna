493da625cc9dcf041d4f219b3608d80d4a0636ab
Ekaitz Torregrosa <ekaitz.torregrosa@540deg.com>
2021-05-18 14:07:35 +0200

    [17] - cambios en la vista dailySchedule

--- src/resources/views/pages/dailySchedule.blade.php
+++ src/resources/views/pages/dailySchedule.blade.php
@@ -1704,7 +1704,7 @@
                     <div class="row row-deck py-4">
                         <!-- Course -->
                         <div class="col-md-6 col-lg-4 col-xl-3">
-                            <a class="block block-rounded block-link-pop" href="be_pages_elearning_course.html">
+                            <a class="block block-rounded block-link-pop" href="">
                                 <div class="block-content block-content-full text-center bg-city">
                                     <div class="item item-2x item-circle bg-white-10 py-3 my-3 mx-auto invisible" data-toggle="appear" data-offset="50" data-class="animated fadeIn">
                                         <i class="fab fa-html5 fa-2x text-white-75"></i>
@@ -1714,8 +1714,8 @@
                                     </div>
                                 </div>
                                 <div class="block-content block-content-full">
-                                    <h4 class="h5 mb-1">Learn HTML5 in 10 simple and easy to follow steps</h4>
-                                    <div class="font-size-sm text-muted">November 5, 2015</div>
+                                    <h4 class="h5 mb-1">Routine 1</h4>
+
                                 </div>
                             </a>
                         </div>
@@ -1723,7 +1723,7 @@
 
                         <!-- Course -->
                         <div class="col-md-6 col-lg-4 col-xl-3">
-                            <a class="block block-rounded block-link-pop" href="be_pages_elearning_course.html">
+                            <a class="block block-rounded block-link-pop" href="">
                                 <div class="block-content block-content-full text-center bg-flat">
                                     <div class="item item-2x item-circle bg-white-10 py-3 my-3 mx-auto invisible" data-toggle="appear" data-offset="50" data-class="animated fadeIn">
                                         <i class="fab fa-css3 fa-2x text-white-75"></i>
@@ -1733,8 +1733,8 @@
                                     </div>
                                 </div>
                                 <div class="block-content block-content-full">
-                                    <h4 class="h5 mb-1">Be a pro with CSS3 in only two weeks</h4>
-                                    <div class="font-size-sm text-muted">November 1, 2015</div>
+                                    <h4 class="h5 mb-1">Routine 2</h4>
+
                                 </div>
                             </a>
                         </div>
@@ -1742,7 +1742,7 @@
 
                         <!-- Course -->
                         <div class="col-md-6 col-lg-4 col-xl-3">
-                            <a class="block block-rounded block-link-pop" href="be_pages_elearning_course.html">
+                            <a class="block block-rounded block-link-pop" href="">
                                 <div class="block-content block-content-full text-center bg-amethyst">
                                     <div class="item item-2x item-circle bg-white-10 py-3 my-3 mx-auto invisible" data-toggle="appear" data-offset="50" data-class="animated fadeIn">
                                         <i class="si si-vector fa-2x text-white-75"></i>
@@ -1752,8 +1752,8 @@
                                     </div>
                                 </div>
                                 <div class="block-content block-content-full">
-                                    <h4 class="h5 mb-1">Using SVG is easier than ever</h4>
-                                    <div class="font-size-sm text-muted">October 27, 2015</div>
+                                    <h4 class="h5 mb-1">Routine 3</h4>
+
                                 </div>
                             </a>
                         </div>
@@ -1761,140 +1761,7 @@
 
                         <!-- Course -->
                         <div class="col-md-6 col-lg-4 col-xl-3">
-                            <a class="block block-rounded block-link-pop" href="be_pages_elearning_course.html">
-                                <div class="block-content block-content-full text-center bg-smooth">
-                                    <div class="item item-2x item-circle bg-white-10 py-3 my-3 mx-auto invisible" data-toggle="appear" data-offset="50" data-class="animated fadeIn">
-                                        <i class="si si-rocket fa-2x text-white-75"></i>
-                                    </div>
-                                    <div class="font-size-sm text-white-75">
-                                        <em>18 lessons</em> &bull; <em>25 hours</em>
-                                    </div>
-                                </div>
-                                <div class="block-content block-content-full">
-                                    <h4 class="h5 mb-1">Build a modern web application from scratch</h4>
-                                    <div class="font-size-sm text-muted">October 25, 2015</div>
-                                </div>
-                            </a>
-                        </div>
-                        <!-- END Course -->
-
-                        <!-- Course -->
-                        <div class="col-md-6 col-lg-4 col-xl-3">
-                            <a class="block block-rounded block-link-pop" href="be_pages_elearning_course.html">
-                                <div class="block-content block-content-full text-center bg-default">
-                                    <div class="item item-2x item-circle bg-white-10 py-3 my-3 mx-auto invisible" data-toggle="appear" data-offset="50" data-class="animated fadeIn">
-                                        <span class="font-size-lg font-w700 text-white-75">PS</span>
-                                    </div>
-                                    <div class="font-size-sm text-white-75">
-                                        <em>5 lessons</em> &bull; <em>19 hours</em>
-                                    </div>
-                                </div>
-                                <div class="block-content block-content-full">
-                                    <h4 class="h5 mb-1">UI Design in Photoshop: Tips  &amp; Tricks</h4>
-                                    <div class="font-size-sm text-muted">October 10, 2015</div>
-                                </div>
-                            </a>
-                        </div>
-                        <!-- END Course -->
-
-                        <!-- Course -->
-                        <div class="col-md-6 col-lg-4 col-xl-3">
-                            <a class="block block-rounded block-link-pop" href="be_pages_elearning_course.html">
-                                <div class="block-content block-content-full text-center bg-modern">
-                                    <div class="item item-2x item-circle bg-white-10 py-3 my-3 mx-auto invisible" data-toggle="appear" data-offset="50" data-class="animated fadeIn">
-                                        <i class="si si-user-following fa-2x text-white-75"></i>
-                                    </div>
-                                    <div class="font-size-sm text-white-75">
-                                        <em>9 lessons</em> &bull; <em>10 hours</em>
-                                    </div>
-                                </div>
-                                <div class="block-content block-content-full">
-                                    <h4 class="h5 mb-1">Marketing 101: All the basics</h4>
-                                    <div class="font-size-sm text-muted">October 1, 2015</div>
-                                </div>
-                            </a>
-                        </div>
-                        <!-- END Course -->
-
-                        <!-- Course -->
-                        <div class="col-md-6 col-lg-4 col-xl-3">
-                            <a class="block block-rounded block-link-pop" href="be_pages_elearning_course.html">
-                                <div class="block-content block-content-full text-center bg-warning">
-                                    <div class="item item-2x item-circle bg-white-10 py-3 my-3 mx-auto invisible" data-toggle="appear" data-offset="50" data-class="animated fadeIn">
-                                        <i class="si si-target fa-2x text-white-75"></i>
-                                    </div>
-                                    <div class="font-size-sm text-white-75">
-                                        <em>5 lessons</em> &bull; <em>8 hours</em>
-                                    </div>
-                                </div>
-                                <div class="block-content block-content-full">
-                                    <h4 class="h5 mb-1">How to make your page convert in 5 steps</h4>
-                                    <div class="font-size-sm text-muted">September 19, 2015</div>
-                                </div>
-                            </a>
-                        </div>
-                        <!-- END Course -->
-
-                        <!-- Course -->
-                        <div class="col-md-6 col-lg-4 col-xl-3">
-                            <a class="block block-rounded block-link-pop" href="be_pages_elearning_course.html">
-                                <div class="block-content block-content-full text-center bg-success">
-                                    <div class="item item-2x item-circle bg-white-10 py-3 my-3 mx-auto invisible" data-toggle="appear" data-offset="50" data-class="animated fadeIn">
-                                        <i class="si si-support fa-2x text-white-75"></i>
-                                    </div>
-                                    <div class="font-size-sm text-white-75">
-                                        <em>21 lessons</em> &bull; <em>29 hours</em>
-                                    </div>
-                                </div>
-                                <div class="block-content block-content-full">
-                                    <h4 class="h5 mb-1">How to provide rock star support</h4>
-                                    <div class="font-size-sm text-muted">September 15, 2015</div>
-                                </div>
-                            </a>
-                        </div>
-                        <!-- END Course -->
-
-                        <!-- Course -->
-                        <div class="col-md-6 col-lg-4 col-xl-3">
-                            <a class="block block-rounded block-link-pop" href="be_pages_elearning_course.html">
-                                <div class="block-content block-content-full text-center bg-info">
-                                    <div class="item item-2x item-circle bg-white-10 py-3 my-3 mx-auto invisible" data-toggle="appear" data-offset="50" data-class="animated fadeIn">
-                                        <i class="si si-social-twitter fa-2x text-white-75"></i>
-                                    </div>
-                                    <div class="font-size-sm text-white-75">
-                                        <em>7 lessons</em> &bull; <em>6 hours</em>
-                                    </div>
-                                </div>
-                                <div class="block-content block-content-full">
-                                    <h4 class="h5 mb-1">Social marketing the right way</h4>
-                                    <div class="font-size-sm text-muted">September 7, 2015</div>
-                                </div>
-                            </a>
-                        </div>
-                        <!-- END Course -->
-
-                        <!-- Course -->
-                        <div class="col-md-6 col-lg-4 col-xl-3">
-                            <a class="block block-rounded block-link-pop" href="be_pages_elearning_course.html">
-                                <div class="block-content block-content-full text-center bg-danger">
-                                    <div class="item item-2x item-circle bg-white-10 py-3 my-3 mx-auto invisible" data-toggle="appear" data-offset="50" data-class="animated fadeIn">
-                                        <i class="si si-briefcase fa-2x text-white-75"></i>
-                                    </div>
-                                    <div class="font-size-sm text-white-75">
-                                        <em>5 lessons</em> &bull; <em>4 hours</em>
-                                    </div>
-                                </div>
-                                <div class="block-content block-content-full">
-                                    <h4 class="h5 mb-1">Project management in 5 simple steps</h4>
-                                    <div class="font-size-sm text-muted">August 28, 2015</div>
-                                </div>
-                            </a>
-                        </div>
-                        <!-- END Course -->
-
-                        <!-- Course -->
-                        <div class="col-md-6 col-lg-4 col-xl-3">
-                            <a class="block block-rounded block-link-pop" href="be_pages_elearning_course.html">
+                            <a class="block block-rounded block-link-pop" href="">
                                 <div class="block-content block-content-full text-center bg-gray-dark">
                                     <div class="item item-2x item-circle bg-white-10 py-3 my-3 mx-auto invisible" data-toggle="appear" data-offset="50" data-class="animated fadeIn">
                                         <i class="si si-camcorder fa-2x text-white-75"></i>
@@ -1904,31 +1771,14 @@
                                     </div>
                                 </div>
                                 <div class="block-content block-content-full">
-                                    <h4 class="h5 mb-1">Learn how to create better screencasts</h4>
-                                    <div class="font-size-sm text-muted">August 19, 2015</div>
+                                    <h4 class="h5 mb-1">Routine 4</h4>
+
                                 </div>
                             </a>
                         </div>
                         <!-- END Course -->
 
-                        <!-- Course -->
-                        <div class="col-md-6 col-lg-4 col-xl-3">
-                            <a class="block block-rounded block-link-pop" href="be_pages_elearning_course.html">
-                                <div class="block-content block-content-full text-center bg-primary">
-                                    <div class="item item-2x item-circle bg-white-10 py-3 my-3 mx-auto invisible" data-toggle="appear" data-offset="50" data-class="animated fadeIn">
-                                        <i class="si si-chemistry fa-2x text-white-75"></i>
-                                    </div>
-                                    <div class="font-size-sm text-white-75">
-                                        <em>12 lessons</em> &bull; <em>7 hours</em>
-                                    </div>
-                                </div>
-                                <div class="block-content block-content-full">
-                                    <h4 class="h5 mb-1">Learn how to create jQuery plugins</h4>
-                                    <div class="font-size-sm text-muted">August 5, 2015</div>
-                                </div>
-                            </a>
-                        </div>
-                        <!-- END Course -->
+
                     </div>
                 </div>
                 <!-- END Page Content -->
