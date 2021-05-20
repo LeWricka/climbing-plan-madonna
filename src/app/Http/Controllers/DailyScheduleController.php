6a8dda189adcf14ac4d7d213c90d46949d05e96c
Ekaitz Torregrosa <ekaitz.torregrosa@540deg.com>
2021-05-20 08:16:45 +0200

    [17] - pr rama 16_main_screen_calendar

--- src/app/Http/Controllers/DailyScheduleController.php
+++ src/app/Http/Controllers/DailyScheduleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller;
 use App\Models\Routine;
+use DateTime;
 
 class DailyScheduleController extends Controller
 {
@@ -19,7 +20,7 @@ class DailyScheduleController extends Controller
         $allRroutines = $this->getAllRoutines();
         $routinesOfDay = [];
         foreach ($allRroutines as $routine){
-            if ($routine->getStart() == $date){
+            if ($routine->getStart()->format('Y-m-d') == $date){
                 array_push($routinesOfDay, $routine);
             }
         }
@@ -28,11 +29,11 @@ class DailyScheduleController extends Controller
     }
     public function getAllRoutines(){
         return [
-            new Routine("First Routine", "2021-05-14", "2021-05-16", "#e04f1a"),
-            new Routine("First Routine 2.0", "2021-05-14", "2021-05-16", "#82b54b"),
-            new Routine("Second Routine", "2021-05-17", "2021-05-19", "#82b54b"),
-            new Routine("Third Routine", "2021-05-20", "2021-05-20", "#ffb119"),
-            new Routine("Final Routine", "2021-05-22", "2021-05-24", "#e04f1a")
+            new Routine("First Routine", new DateTime('2021-05-14'), new DateTime('2021-05-16'), "#e04f1a"),
+            new Routine("First Routine 2.0", new DateTime('2021-05-14'), new DateTime('2021-05-16'), "#82b54b"),
+            new Routine("Second Routine", new DateTime('2021-05-17'), new DateTime('2021-05-19'), "#82b54b"),
+            new Routine("Third Routine", new DateTime('2021-05-20'), new DateTime('2021-05-20'), "#ffb119"),
+            new Routine("Final Routine", new DateTime('2021-05-22'), new DateTime('2021-05-24'), "#e04f1a")
         ];
     }
 }
