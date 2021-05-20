c7a0208a8519eed6fa7ffeeeca1415afc3911299
Ekaitz Torregrosa <ekaitz.torregrosa@540deg.com>
2021-05-20 16:27:52 +0200

    [19] - Alineado Apartado Charts

--- src/resources/views/components/navbar.blade.php
+++ src/resources/views/components/navbar.blade.php
@@ -107,7 +107,7 @@
                         <span class="nav-main-link-name">Calendar</span>
                     </a>
                 </li>
-                <li class="nav-main">
+                <li class="nav-main-item">
                     <a class="nav-main-link" href="#">
                         <i class="nav-main-link-icon si si-chart"></i>
                         <span class="nav-main-link-name">Charts</span>
