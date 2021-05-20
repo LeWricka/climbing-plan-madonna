afa66ef38a25f74c0f980994a3f02045ccf03855
Joel Hazas <joelhazas@MacBook-Pro-de-Pablo.local>
2021-05-20 14:09:12 +0200

    [14] - Vista actualizada con validación

--- src/resources/views/auth/forgot-password.blade.php
+++ src/resources/views/auth/forgot-password.blade.php
@@ -43,170 +43,179 @@
     <!-- <link rel="stylesheet" id="css-theme" href="assets/css/themes/amethyst.min.css"> -->
     <!-- END Stylesheets -->
 </head>
-    <body>
-        <!-- Page Container -->
-        <!--
-            Available classes for #page-container:
+<body>
+<!-- Page Container -->
+<!--
+    Available classes for #page-container:
 
-        GENERIC
+GENERIC
 
-            'enable-cookies'                            Remembers active color theme between pages (when set through color theme helper Template._uiHandleTheme())
+    'enable-cookies'                            Remembers active color theme between pages (when set through color theme helper Template._uiHandleTheme())
 
-        SIDEBAR & SIDE OVERLAY
+SIDEBAR & SIDE OVERLAY
 
-            'sidebar-r'                                 Right Sidebar and left Side Overlay (default is left Sidebar and right Side Overlay)
-            'sidebar-mini'                              Mini hoverable Sidebar (screen width > 991px)
-            'sidebar-o'                                 Visible Sidebar by default (screen width > 991px)
-            'sidebar-o-xs'                              Visible Sidebar by default (screen width < 992px)
-            'sidebar-dark'                              Dark themed sidebar
+    'sidebar-r'                                 Right Sidebar and left Side Overlay (default is left Sidebar and right Side Overlay)
+    'sidebar-mini'                              Mini hoverable Sidebar (screen width > 991px)
+    'sidebar-o'                                 Visible Sidebar by default (screen width > 991px)
+    'sidebar-o-xs'                              Visible Sidebar by default (screen width < 992px)
+    'sidebar-dark'                              Dark themed sidebar
 
-            'side-overlay-hover'                        Hoverable Side Overlay (screen width > 991px)
-            'side-overlay-o'                            Visible Side Overlay by default
+    'side-overlay-hover'                        Hoverable Side Overlay (screen width > 991px)
+    'side-overlay-o'                            Visible Side Overlay by default
 
-            'enable-page-overlay'                       Enables a visible clickable Page Overlay (closes Side Overlay on click) when Side Overlay opens
+    'enable-page-overlay'                       Enables a visible clickable Page Overlay (closes Side Overlay on click) when Side Overlay opens
 
-            'side-scroll'                               Enables custom scrolling on Sidebar and Side Overlay instead of native scrolling (screen width > 991px)
+    'side-scroll'                               Enables custom scrolling on Sidebar and Side Overlay instead of native scrolling (screen width > 991px)
 
-        HEADER
+HEADER
 
-            ''                                          Static Header if no class is added
-            'page-header-fixed'                         Fixed Header
+    ''                                          Static Header if no class is added
+    'page-header-fixed'                         Fixed Header
 
-        HEADER STYLE
+HEADER STYLE
 
-            ''                                          Light themed Header
-            'page-header-dark'                          Dark themed Header
+    ''                                          Light themed Header
+    'page-header-dark'                          Dark themed Header
 
-        MAIN CONTENT LAYOUT
+MAIN CONTENT LAYOUT
 
-            ''                                          Full width Main Content if no class is added
-            'main-content-boxed'                        Full width Main Content with a specific maximum width (screen width > 1200px)
-            'main-content-narrow'                       Full width Main Content with a percentage width (screen width > 1200px)
-        -->
-        <div id="page-container">
+    ''                                          Full width Main Content if no class is added
+    'main-content-boxed'                        Full width Main Content with a specific maximum width (screen width > 1200px)
+    'main-content-narrow'                       Full width Main Content with a percentage width (screen width > 1200px)
+-->
+<div id="page-container">
 
-            <!-- Main Container -->
-            <main id="main-container">
-                <!-- Page Content -->
-                <div class="bg-image" style="background-image: url({{('media/photos/leftSideImage.jpg')}});"/>
+    <!-- Main Container -->
+    <main id="main-container">
+        <!-- Page Content -->
+        <div class="bg-image" style="background-image: url({{('media/photos/leftSideImage.jpg')}});"/>
 
-                <div class="row no-gutters"
-                     style="background-image: url('{{ asset('assets/images/leftSideImage.png')}}');">
-                    <!-- Meta Info Section -->
-                    <div class="hero-static col-lg-4 d-none d-lg-flex flex-column justify-content-center">
-                        <div class="p-4 p-xl-5 flex-grow-1 d-flex align-items-center">
-                            <div class="w-100">
-                                <a class="link-fx font-w600 font-size-h2 text-white" href="index.html">
-                                    ClimbingPlan
-                                </a>
-                                <p class="text-white-75 mr-xl-8 mt-2">
-                                    Improve your climbing with the best climbing trainning programs
-                                </p>
-                            </div>
-                        </div>
-                        <div class="p-4 p-xl-5 d-xl-flex justify-content-between align-items-center font-size-sm">
-                            <p class="font-w500 text-white-50 mb-0">
-                                <strong>ClimbingPlan</strong> &copy; <span data-toggle="year-copy"></span>
-                            </p>
-                            <ul class="list list-inline mb-0 py-2">
-                                <li class="list-inline-item">
-                                    <a class="text-white-75 font-w500" href="javascript:void(0)">Legal</a>
-                                </li>
-                                <li class="list-inline-item">
-                                    <a class="text-white-75 font-w500" href="javascript:void(0)">Contact</a>
-                                </li>
-                                <li class="list-inline-item">
-                                    <a class="text-white-75 font-w500" href="javascript:void(0)">Terms</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- END Meta Info Section -->
-
-                        <!-- Main Section -->
-                        <div class="hero-static col-lg-8 d-flex flex-column align-items-center bg-white">
-                            <div class="p-3 w-100 d-lg-none text-center">
-                                <a class="link-fx font-w600 font-size-h3 text-dark" href="index.html">
-                                    Climbing<span class="font-w400">Plan</span>
-                                </a>
-                            </div>
-                            <div class="p-4 w-100 flex-grow-1 d-flex align-items-center">
-                                <div class="w-100">
-                                    <!-- Header -->
-                                    <div class="text-center mb-5">
-                                        <p class="mb-3">
-                                            <i class="fa fa-2x fa-circle-notch text-primary-light"></i>
-                                        </p>
-                                        <h1 class="font-w700 mb-2">
-                                            Password Reminder
-                                        </h1>
-                                        <h2 class="font-size-base text-muted">
-                                            Please provide your account’s email and we will send you your password.
-                                        </h2>
-                                    </div>
-                                    <!-- END Header -->
-
-                                    <!-- Reminder Form -->
-                                    <!-- jQuery Validation (.js-validation-reminder class is initialized in js/pages/op_auth_reminder.min.js which was auto compiled from _js/pages/op_auth_reminder.js) -->
-                                    <!-- For more info and examples you can check out https://github.com/jzaefferer/jquery-validation -->
-                                    <div class="row no-gutters justify-content-center">
-                                        <div class="col-sm-8 col-xl-4">
-                                            <form method="POST" action="{{ route('password.email') }}">
-                                                <div class="form-group">
-                                                    <input type="text" class="form-control form-control-lg form-control-alt py-4" id="reminder-credential" name="reminder-credential" placeholder="Username or Email">
-                                                </div>
-                                                <div class="form-group text-center mb-0">
-                                                    <button type="submit" class="btn btn-lg btn-alt-primary">
-                                                        <i class="fa fa-fw fa-envelope mr-1 opacity-50"></i> Send Mail
-                                                    </button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                    <!-- END Reminder Form -->
-                                </div>
-                            </div>
-                            <div class="px-4 py-3 w-100 d-lg-none d-flex flex-column flex-sm-row justify-content-between font-size-sm text-center text-sm-left">
-                                <p class="font-w500 text-black-50 py-2 mb-0">
-                                    <strong>ClimbingPlan</strong> &copy; <span data-toggle="year-copy"></span>
-                                </p>
-                                <ul class="list list-inline py-2 mb-0">
-                                    <li class="list-inline-item">
-                                        <a class="text-muted font-w500" href="javascript:void(0)">Legal</a>
-                                    </li>
-                                    <li class="list-inline-item">
-                                        <a class="text-muted font-w500" href="javascript:void(0)">Contact</a>
-                                    </li>
-                                    <li class="list-inline-item">
-                                        <a class="text-muted font-w500" href="javascript:void(0)">Terms</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <!-- END Main Section -->
+        <div class="row no-gutters"
+             style="background-image: url('{{ asset('assets/images/leftSideImage.png')}}');">
+            <!-- Meta Info Section -->
+            <div class="hero-static col-lg-4 d-none d-lg-flex flex-column justify-content-center">
+                <div class="p-4 p-xl-5 flex-grow-1 d-flex align-items-center">
+                    <div class="w-100">
+                        <a class="link-fx font-w600 font-size-h2 text-white" href="../">
+                            ClimbingPlan
+                        </a>
+                        <p class="text-white-75 mr-xl-8 mt-2">
+                            Improve your climbing with the best climbing training programs
+                        </p>
                     </div>
                 </div>
-                <!-- END Page Content -->
-            </main>
-            <!-- END Main Container -->
+                <div class="p-4 p-xl-5 d-xl-flex justify-content-between align-items-center font-size-sm">
+                    <p class="font-w500 text-white-50 mb-0">
+                        <strong>ClimbingPlan</strong> &copy; <span data-toggle="year-copy"></span>
+                    </p>
+                    <ul class="list list-inline mb-0 py-2">
+                        <li class="list-inline-item">
+                            <a class="text-white-75 font-w500" href="javascript:void(0)">Legal</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="text-white-75 font-w500" href="javascript:void(0)">Contact</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="text-white-75 font-w500" href="javascript:void(0)">Terms</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- END Meta Info Section -->
+
+            <!-- Main Section -->
+            <div class="hero-static col-lg-8 d-flex flex-column align-items-center bg-white">
+                <div class="p-3 w-100 d-lg-none text-center">
+                    <a class="link-fx font-w600 font-size-h3 text-dark" href="../">
+                        Climbing<span class="font-w400">Plan</span>
+                    </a>
+                </div>
+                <div class="p-4 w-100 flex-grow-1 d-flex align-items-center">
+                    <div class="w-100">
+                        <!-- Header -->
+                        <div class="text-center mb-5">
+                            <p class="mb-3">
+                                <i class="fa fa-2x fa-circle-notch text-primary-light"></i>
+                            </p>
+                            <h1 class="font-w700 mb-2">
+                                Password Reminder
+                            </h1>
+                            <h2 class="font-size-base text-muted">
+                                Please provide your account’s email and we will send you your password.
+                            </h2>
+                        </div>
+                        <!-- END Header -->
+
+                        <!-- Reminder Form -->
+                        <!-- jQuery Validation (.js-validation-reminder class is initialized in js/pages/op_auth_reminder.min.js which was auto compiled from _js/pages/op_auth_reminder.js) -->
+                        <!-- For more info and examples you can check out https://github.com/jzaefferer/jquery-validation -->
+                        <div class="row no-gutters justify-content-center">
+                            <div class="col-sm-8 col-xl-4">
+                                <form method="POST" action="{{ route('password.email') }}">
+                                    @csrf
+                                    <div class="form-group">
+                                        <input type="text"
+                                               class="form-control form-control-lg form-control-alt py-4 @error('email') is-invalid @enderror"
+                                               id="email" name="email" placeholder="Email">
+                                        @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group text-center mb-0">
+                                        <button type="submit" class="btn btn-lg btn-alt-primary">
+                                            <i class="fa fa-fw fa-envelope mr-1 opacity-50"></i> Send Mail
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <!-- END Reminder Form -->
+                    </div>
+                </div>
+                <div
+                    class="px-4 py-3 w-100 d-lg-none d-flex flex-column flex-sm-row justify-content-between font-size-sm text-center text-sm-left">
+                    <p class="font-w500 text-black-50 py-2 mb-0">
+                        <strong>ClimbingPlan</strong> &copy; <span data-toggle="year-copy"></span>
+                    </p>
+                    <ul class="list list-inline py-2 mb-0">
+                        <li class="list-inline-item">
+                            <a class="text-muted font-w500" href="javascript:void(0)">Legal</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="text-muted font-w500" href="javascript:void(0)">Contact</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="text-muted font-w500" href="javascript:void(0)">Terms</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- END Main Section -->
         </div>
-        <!-- END Page Container -->
+</div>
+<!-- END Page Content -->
+</main>
+<!-- END Main Container -->
+</div>
+<!-- END Page Container -->
 
-        <!--
-            OneUI JS Core
+<!--
+    OneUI JS Core
 
-            Vital libraries and plugins used in all pages. You can choose to not include this file if you would like
-            to handle those dependencies through webpack. Please check out assets/_js/main/bootstrap.js for more info.
+    Vital libraries and plugins used in all pages. You can choose to not include this file if you would like
+    to handle those dependencies through webpack. Please check out assets/_js/main/bootstrap.js for more info.
 
-            If you like, you could also include them separately directly from the assets/js/core folder in the following
-            order. That can come in handy if you would like to include a few of them (eg jQuery) from a CDN.
+    If you like, you could also include them separately directly from the assets/js/core folder in the following
+    order. That can come in handy if you would like to include a few of them (eg jQuery) from a CDN.
 
-            assets/js/core/jquery.min.js
-            assets/js/core/bootstrap.bundle.min.js
-            assets/js/core/simplebar.min.js
-            assets/js/core/jquery-scrollLock.min.js
-            assets/js/core/jquery.appear.min.js
-            assets/js/core/js.cookie.min.js
-        -->
-    </body>
+    assets/js/core/jquery.min.js
+    assets/js/core/bootstrap.bundle.min.js
+    assets/js/core/simplebar.min.js
+    assets/js/core/jquery-scrollLock.min.js
+    assets/js/core/jquery.appear.min.js
+    assets/js/core/js.cookie.min.js
+-->
+</body>
 </html>
