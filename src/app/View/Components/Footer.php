0f80934df6b59361aac94a10a2072e60b959cb20
Ekaitz Torregrosa <ekaitz.torregrosa@540deg.com>
2021-05-21 15:13:41 +0200

    [19] - Implementados componentes Head y Footer, pr revisada

--- src/app/View/Components/Footer.php
+++ src/app/View/Components/Footer.php
@@ -4,10 +4,10 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class Navbar extends Component
+class Footer extends Component
 {
     public function render()
     {
-        return view('components.navbar');
+        return view('components.footer');
     }
 }
--- src/app/View/Components/Head.php
+++ src/app/View/Components/Head.php
@@ -4,10 +4,10 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class Navbar extends Component
+class Head extends Component
 {
     public function render()
     {
-        return view('components.navbar');
+        return view('components.head');
     }
 }
