b318676d406054eafd1b037eef59d249d5efea0f
Iker Mariñelarena <iker@540deg.com>
2021-06-27 20:35:10 +0200

    [] - Adjust login

--- src/app/Http/Controllers/Auth/RegisterController.php
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -4,18 +4,15 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use Core\Users\Domain\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
 use Kreait\Firebase\Auth as FirebaseAuth;
-use Kreait\Firebase\Exception\FirebaseException;
 
 class RegisterController extends Controller
 {
     use RegistersUsers;
+
     protected $auth;
 
     /**
@@ -24,17 +21,15 @@ class RegisterController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
-    public function __construct(FirebaseAuth $auth) {
+
+    public function __construct(FirebaseAuth $auth)
+    {
         $this->middleware('guest');
         $this->auth = $auth;
     }
-    protected function validator(array $data) {
-        return Validator::make($data, [
-            'email' => ['required', 'string', 'email', 'max:255'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
-    }
-    protected function register(Request $request) {
+
+    protected function register(Request $request)
+    {
         $this->validator($request->all())->validate();
         $userProperties = [
             'email' => $request->input('email'),
@@ -44,5 +39,13 @@ class RegisterController extends Controller
         ];
         $createdUser = $this->auth->createUser($userProperties);
         return redirect()->route('login');
+    }
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
     }
 }
